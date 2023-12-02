abbbecb8803fbda20f79e9fb7f6f6ab1b0330b63
Toon Tamil India <121746774+ToonTamilIndia@users.noreply.github.com>
2023-12-02 21:36:46 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,1 +1,185 @@
-const infoapi="https://techzapi2.vercel.app/anime/gogoanime/info/",epapi="https://api.techzbots1.workers.dev/gogo/episodes/",searchapi="https://techzapi2.vercel.app/meta/anilist/",anilistinfoapi="https://techzapi2.vercel.app/meta/anilist/info/",gogosearchapi="https://techzapi2.vercel.app/anime/gogoanime/";async function getJson(e){try{let t=await axios.get(e);return t.data}catch(l){console.error(l)}}function getGenreHtml(e){let t="";for(let l=0;l<e.length;l++)t+=`<a>${e[l]}</a>`;return t}async function RefreshLazyLoader(){let e=new IntersectionObserver((e,t)=>{e.forEach(e=>{if(e.isIntersecting){let t=e.target;t.src=t.dataset.src}})}),t=document.querySelectorAll("img.lzy_img");t.forEach(t=>{e.observe(t)})}function getAnilistTitle(e){return null!=e.userPreferred?e.userPreferred:null!=e.english?e.english:null!=e.romaji?e.romaji:null!=e.native?e.native:"Unknown"}function getAnilistOtherTitle(e,t){return null!=e.userPreferred&&e.userPreferred!=t?e.userPreferred:null!=e.english&&e.english!=t?e.english:null!=e.romaji&&e.romaji!=t?e.romaji:null!=e.native&&e.native!=t?e.native:"Unknown"}async function getAnimeInfo(e){try{let t=await getJson(infoapi+e);document.documentElement.innerHTML=document.documentElement.innerHTML.replaceAll("TITLE",t.title).replaceAll("IMG",t.image).replaceAll("LANG",t.subOrDub.toUpperCase()).replaceAll("TYPE",t.type).replaceAll("URL",window.location).replaceAll("SYNOPSIS",t.description).replaceAll("OTHER",t.otherName).replaceAll("TOTAL",t.totalEpisodes).replaceAll("YEAR",t.releaseDate).replaceAll("STATUS",t.status).replaceAll("GENERES",getGenreHtml(t.genres)),document.getElementById("main-content").style.display="block",document.getElementById("load").style.display="none";let l=t.title;getEpList(e).then(e=>{console.log("Episode list loaded"),getRecommendations(l).then(e=>{RefreshLazyLoader(),console.log("Anime Recommendations loaded")})})}catch(a){await getSearchGogoAnimeInfo(e)}}async function getSearchGogoAnimeInfo(e){try{let t=await getJson("https://techzapi2.vercel.app/anime/gogoanime/"+e),l=t.results[0].id;t=await getJson(infoapi+l),document.documentElement.innerHTML=document.documentElement.innerHTML.replaceAll("TITLE",t.title).replaceAll("IMG",t.image).replaceAll("LANG",t.subOrDub.toUpperCase()).replaceAll("TYPE",t.type).replaceAll("URL",window.location).replaceAll("SYNOPSIS",t.description).replaceAll("OTHER",t.otherName).replaceAll("TOTAL",t.totalEpisodes).replaceAll("YEAR",t.releaseDate).replaceAll("STATUS",t.status).replaceAll("GENERES",getGenreHtml(t.genres)),document.getElementById("main-content").style.display="block",document.getElementById("load").style.display="none",getEpList(l).then(t=>{console.log("Episode list loaded"),getRecommendations(e).then(e=>{RefreshLazyLoader(),console.log("Anime Recommendations loaded")})})}catch(a){await getAnilistAnimeInfo(e)}}async function getAnilistAnimeInfo(e){try{let t=await getJson(searchapi+e),l=t.results[0].id;t=await getJson(anilistinfoapi+l);let a=getAnilistTitle(t.title);document.documentElement.innerHTML=document.documentElement.innerHTML.replaceAll("TITLE",a).replaceAll("IMG",t.image).replaceAll("LANG",t.subOrDub.toUpperCase()).replaceAll("TYPE",t.type).replaceAll("URL",window.location).replaceAll("SYNOPSIS",t.description).replaceAll("OTHER",getAnilistOtherTitle(t.title,a)).replaceAll("TOTAL",t.totalEpisodes).replaceAll("YEAR",t.releaseDate).replaceAll("STATUS",t.status).replaceAll("GENERES",getGenreHtml(t.genres)),document.getElementById("main-content").style.display="block",document.getElementById("load").style.display="none";let n=t.recommendations,s="";for(i=0;i<n.length;i++){let r=n[i],o=r.title.userPreferred;s+=`<a href="./anime.html?anime=${o}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">${r.rating}</div><div class="dubb dubb2">${r.type}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="./logo/loading2.gif" data-src="${r.image}"> </div><div class="la-details"> <h3>${o}</h3> <div id="extra"> <span>${r.status}</span> <span class="dot"></span> <span>${r.episodes}</span> </div></div></div></a>`}document.getElementById("latest2").innerHTML=s,document.getElementById("ephtmldiv").innerHTML='<a class="ep-btn">Anime Name Not Found On GogoAnime, Try Searching With A Different Name...</a>'}catch(c){console.log(c)}}async function getEpList(e){let t=await getJson("https://api.techzbots1.workers.dev/gogo/episodes/"+e),l=Number(t.total),a="";for(let n=0;n<l;n++)a+=`<a class="ep-btn" href="./episode.html?anime=${e}&episode=${n+1}">${n+1}</a>`;document.getElementById("ephtmldiv").innerHTML=a}async function getRecommendations(e){let t=await getJson(searchapi+e),l=t.results[0].id;t=await getJson(anilistinfoapi+l);let a=t.recommendations,n="";for(i=0;i<a.length;i++){let s=a[i],r=s.title.userPreferred;n+=`<a href="./anime.html?anime=${r}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">${s.rating}</div><div class="dubb dubb2">${s.type}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="./logo/loading2.gif" data-src="${s.image}"> </div><div class="la-details"> <h3>${r}</h3> <div id="extra"> <span>${s.status}</span> <span class="dot"></span> <span>${s.episodes}</span> </div></div></div></a>`}document.getElementById("latest2").innerHTML=n}const queryString=window.location.search,urlParams=new URLSearchParams(queryString);null==urlParams.get("anime")&&(window.location="./index.html"),getAnimeInfo(urlParams.get("anime")).then(e=>{RefreshLazyLoader(),console.log("Anime Info loaded")});
+// Api urls
+
+const animeapi = "https://api.anime-dex.workers.dev/anime/";
+const recommendationsapi = "https://api.anime-dex.workers.dev/recommendations/";
+
+// Usefull functions
+
+async function getJson(url) {
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (errors) {
+        console.error(errors);
+    }
+}
+
+function getGenreHtml(genres) {
+    let ghtml = "";
+    for (let i = 0; i < genres.length; i++) {
+        ghtml += `<a>${genres[i].trim()}</a>`;
+    }
+    return ghtml;
+}
+
+async function RefreshLazyLoader() {
+    const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const lazyImage = entry.target;
+                lazyImage.src = lazyImage.dataset.src;
+            }
+        });
+    });
+    const arr = document.querySelectorAll("img.lzy_img");
+    arr.forEach((v) => {
+        imageObserver.observe(v);
+    });
+}
+
+function getAnilistTitle(title) {
+    if (title["userPreferred"] != null) {
+        return title["userPreferred"];
+    } else if (title["english"] != null) {
+        return title["english"];
+    } else if (title["romaji"] != null) {
+        return title["romaji"];
+    } else if (title["native"] != null) {
+        return title["native"];
+    } else {
+        return "Unknown";
+    }
+}
+
+function getAnilistOtherTitle(title, current) {
+    if (title["userPreferred"] != null && title["userPreferred"] != current) {
+        return title["userPreferred"];
+    } else if (title["english"] != null && title["english"] != current) {
+        return title["english"];
+    } else if (title["romaji"] != null && title["romaji"] != current) {
+        return title["romaji"];
+    } else if (title["native"] != null && title["native"] != current) {
+        return title["native"];
+    } else {
+        return "Unknown";
+    }
+}
+
+// Function to get anime info from gogo id
+async function loadAnimeFromGogo(data) {
+    document.documentElement.innerHTML = document.documentElement.innerHTML
+        .replaceAll("TITLE", data["name"])
+        .replaceAll("IMG", data["image"])
+        .replaceAll("LANG", "EP " + data["episodes"].length)
+        .replaceAll("TYPE", data["type"])
+        .replaceAll("URL", window.location)
+        .replaceAll("SYNOPSIS", data["plot_summary"])
+        .replaceAll("OTHER", data["other_name"])
+        .replaceAll("TOTAL", data["episodes"].length)
+        .replaceAll("YEAR", data["released"])
+        .replaceAll("STATUS", data["status"])
+        .replaceAll("GENERES", getGenreHtml(data["genre"].split(",")));
+
+    document.getElementById("main-content").style.display = "block";
+    document.getElementById("load").style.display = "none";
+    document.getElementById("watch-btn").href =
+        "./episode.html?anime=" +
+        data["episodes"][0][1].split("-episode-")[0] +
+        "&episode=" +
+        data["episodes"][0][0];
+    const anime_title = data["title"];
+
+    console.log("Anime Info loaded");
+    RefreshLazyLoader();
+
+    getEpList(data["id"], data["episodes"]).then((data) => {
+        console.log("Episode list loaded");
+
+        getRecommendations(anime_title).then((data) => {
+            RefreshLazyLoader();
+            console.log("Anime Recommendations loaded");
+        });
+    });
+}
+
+// Function to get anime info from anilist search
+async function loadAnimeFromAnilist(data) {
+    const title = getAnilistTitle(data["title"]);
+
+    document.documentElement.innerHTML = document.documentElement.innerHTML
+        .replaceAll("TITLE", title)
+        .replaceAll("IMG", data["coverImage"]["large"])
+        .replaceAll("LANG", "EP " + data["episodes"])
+        .replaceAll("TYPE", data["format"])
+        .replaceAll("URL", window.location)
+        .replaceAll("SYNOPSIS", data["description"])
+        .replaceAll("OTHER", getAnilistOtherTitle(data["title"], title))
+        .replaceAll("TOTAL", "EP " + data["episodes"])
+        .replaceAll("YEAR", data["seasonYear"])
+        .replaceAll("STATUS", data["status"])
+        .replaceAll("GENERES", getGenreHtml(data["genres"]));
+
+    document.getElementById("main-content").style.display = "block";
+    document.getElementById("load").style.display = "none";
+
+    console.log("Anime Info loaded");
+
+    const recommendations = data["recommendations"];
+    let rechtml = "";
+
+    for (i = 0; i < recommendations.length; i++) {
+        let anime = recommendations[i];
+        let title = anime["title"]["userPreferred"];
+        rechtml += `<a href="./anime.html?anime=${title}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">${anime["meanScore"]} / 100</div><div class="dubb dubb2">${anime["format"]}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/img/loading.gif" data-src="${anime["coverImage"]["large"]}"> </div><div class="la-details"> <h3>${title}</h3> <div id="extra"> <span>${anime["status"]}</span> <span class="dot"></span> <span>EP ${anime["episodes"]}</span> </div></div></div></a>`;
+    }
+    document.getElementById("latest2").innerHTML = rechtml;
+
+    document.getElementById("ephtmldiv").innerHTML =
+        '<a class="ep-btn">Anime Name Not Found On GogoAnime, Try Searching With A Different Name...</a>';
+
+    RefreshLazyLoader();
+    console.log("Anime Recommendations loaded");
+}
+
+// Function to get episode list
+async function getEpList(anime_id, total) {
+    let ephtml = "";
+
+    for (let i = 0; i < total.length; i++) {
+        x = total[i][1].split("-episode-");
+        ephtml += `<a class="ep-btn" href="./episode.html?anime=${x[0]}&episode=${x[1]}">${x[1]}</a>`;
+    }
+    document.getElementById("ephtmldiv").innerHTML = ephtml;
+}
+
+// Function to get anime recommendations
+async function getRecommendations(anime_title) {
+    const data = await getJson(recommendationsapi + anime_title);
+    const recommendations = data["results"];
+    let rechtml = "";
+
+    for (i = 0; i < recommendations.length; i++) {
+        let anime = recommendations[i];
+        let title = anime["title"]["userPreferred"];
+        rechtml += `<a href="./anime.html?anime=${title}"><div class="poster la-anime"> <div id="shadow1" class="shadow"> <div class="dubb">${anime["meanScore"]} / 100</div><div class="dubb dubb2">${anime["format"]}</div></div><div id="shadow2" class="shadow"> <img class="lzy_img" src="https://cdn.jsdelivr.net/gh/TechShreyash/AnimeDex@main/static/img/loading.gif" data-src="${anime["coverImage"]["large"]}"> </div><div class="la-details"> <h3>${title}</h3> <div id="extra"> <span>${anime["status"]}</span> <span class="dot"></span> <span>EP ${anime["episodes"]}</span> </div></div></div></a>`;
+    }
+    document.getElementById("latest2").innerHTML = rechtml;
+}
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if (urlParams.get("anime") == null) {
+    window.location = "./index.html";
+}
+
+//Running functions
+getJson(animeapi + urlParams.get("anime")).then((data) => {
+    data = data["results"];
+
+    if (data.source == "gogoanime") {
+        loadAnimeFromGogo(data);
+    } else if (data.source == "anilist") {
+        loadAnimeFromAnilist(data);
+    }
+});
